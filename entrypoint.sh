6b6087b2cf697443a36f52ef22fd0615ad27b044
deep-lee <1136855714@qq.com>
2023-02-05 15:10:12 +0800

    Update entrypoint.sh
    update env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及伪装路径、哪吒面板参数，请自行修改. (注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID='de04add9-5c68-8bab-950c-08cd5320df18'
-VMESS_WSPATH='/vmess'
-VLESS_WSPATH='/vless'
-TROJAN_WSPATH='/trojan'
-SS_WSPATH='/shadowsocks'
+UUID='6a4ff628-8825-4fef-bc59-8eb99714dd11'
+VMESS_WSPATH='/vm'
+VLESS_WSPATH='/vl'
+TROJAN_WSPATH='/tr'
+SS_WSPATH='/ss'
 NEZHA_SERVER=''
 NEZHA_PORT=''
 NEZHA_KEY=''
